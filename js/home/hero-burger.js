086cd80ef3d063cbba99b5d0438207ac08b7bfd4
diasSarekpera <154084819+diasSarekpera@users.noreply.github.com>
2025-11-26 03:37:12 -0800

    Delete js/home directory

--- js/home/hero-burger.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const overlayMenu = document.querySelector('.overlay-mobile-menu');
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    overlayMenu.classList.toggle('active');
-});
